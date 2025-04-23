9c27c46d2c5d3ef8cf61ef871b356e5232129e8e
MUHAMMAD shahroz abbas <32800432+shahroz8899@users.noreply.github.com>
2025-04-23 16:32:17 +0300

    Delete Master_node_script directory

--- Master_node_script/docker_image_processor/run_process.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Loop through all .jpg files in the /images directory
-for img in /images/*.jpg; do
-  if [ -f "$img" ]; then
-    echo "Processing $img"
-    python /script/process_image.py "$img"
-  fi
-done
